d6c9ecc19823b8dff8ba7faaae564005f404daec
WebCoder33 <coder33@yandex.ru>
2020-03-31 09:19:39 +0300

    готовы задания 8,9,10

--- person/peopleGenerator.php
+++ person/peopleGenerator.php
@@ -1,19 +1,26 @@
 <?php
 
 require_once 'person.php';
+require_once 'surnameGenerator.php';
+require_once 'nameGenerator.php';
+require_once 'patronymicGenerator.php';
 
 class peopleGenerator {
 
     public function generatePerson() {
 
-        $surname = ['Путин','Греф','Чубайс','Иванов','Петров','Сидоров', 'Мишустин'];
-        $name = ['Иван', 'Петр', 'Степан','Василий','Арнольд','Дмитрий'];
-        $patronimyc = ['Арнольдович','Степанович','Владимирович','Святославович'];
+        $surnameGenerator = new surnameGenerator();
+        $nameGenerator = new nameGenerator();
+        $patronimycGenerator = new patronymicGenerator();
+
+        $surname = $surnameGenerator->generateSurname();
+        $name = $nameGenerator->generateName();
+        $patronimyc = $patronimycGenerator->generatePatronymic();
 
         return new Person([
-            $surname[array_rand($surname, 1)], 
-            $name[array_rand($name, 1)], 
-            $patronimyc[array_rand($patronimyc, 1)], 
+            $surname, 
+            $name, 
+            $patronimyc, 
             rand(25,60)
             ]);
     }
